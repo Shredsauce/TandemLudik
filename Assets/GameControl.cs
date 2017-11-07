637288d42077a9940898c0087ba8f9d574b6d4e8
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-11-07 10:55:38 -0500

    Uniduino test and curved ground

---
+++ Assets/GameControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameControl : MonoBehaviour {
+
+	public GameControl Instance;
+
+	public float curvature = 0.001f;
+
+	[Range(0f, 1f)] public float curveAmount = 0f;
+	public float minCurvature = 0.0001f;
+	public float maxCurvature = 0.03f;
+
+	public Shader curvedShader;
+
+	private List<Material> materials = new List<Material>();
+
+	void Awake () {
+		Instance = this;
+	}
+
+	void Start () {
+		Renderer[] renderers = FindObjectsOfType<Renderer>();
+		for (int i = 0; i < renderers.Length; i++) {
+			if (renderers[i].material.shader == curvedShader) {
+				materials.Add(renderers[i].material);
+			}
+		}
+
+	}
+	
+	void Update () {
+		curvature = Mathf.Lerp(minCurvature, maxCurvature, curveAmount);
+
+		for (int i = 0; i < materials.Count; i++) {
+			materials[i].SetFloat("_Curvature", curvature);
+		}
+	}
+}
---
+++ Assets/MoveBike.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBike : MonoBehaviour {
+
+	public float minTime = 0.2f;
+	public float maxTime = 1f;
+
+	public float minSpeed = -0.4f;
+	public float maxSpeed = 0.4f;
+
+	private float oldSpeed;
+
+	void Start () {
+		StartCoroutine(MoveOverTime());
+	}
+	
+	void Update () {
+		
+	}
+
+	private IEnumerator MoveOverTime () {
+
+		float timer = Random.Range(minTime, maxTime);
+		float speed = Random.Range(minSpeed, maxSpeed);
+
+		float timerDiv = 1 / timer;
+
+		while (timer > 0) {
+			timer -= Time.deltaTime;
+			this.transform.Translate(0, 0, Mathf.Lerp(speed, oldSpeed, 1 - Mathf.InverseLerp(0, 1, timer)) * Time.deltaTime);
+			yield return null;
+		}
+		
+		oldSpeed = speed;
+
+		StartCoroutine(MoveOverTime());
+	}
+}
