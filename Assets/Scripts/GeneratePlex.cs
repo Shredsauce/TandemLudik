97553027a01b4e175d55ace012bb61a000ddffb6
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2019-01-16 10:55:56 -0500

    Procedural building test

---
+++ Assets/Scripts/GeneratePlex.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlex : MonoBehaviour {
+
+    [SerializeField] private GameObject facadePrefab;
+    [SerializeField] private GameObject windowPrefab;
+
+
+    [SerializeField] private float width = 5f;
+    [SerializeField] private float balconyWidth = 3f;
+    [SerializeField] private float foundationHeight = 1f;
+    [SerializeField] private float storyHeight = 2f;
+    [SerializeField] private int stories = 3;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            Debug.Log("Generate plex");
+
+            Create();
+        }
+	}
+
+    public GameObject Create () {
+
+        GameObject plex = new GameObject("Plex");
+
+        GameObject facade = Instantiate(facadePrefab);
+
+        facade.transform.localScale = new Vector3(
+            width,
+            (storyHeight * stories) + foundationHeight,
+            facade.transform.localScale.z
+        );
+        facade.transform.parent = plex.transform;
+
+        for (int i = 0; i < stories; i++) {
+            GameObject window1 = Instantiate(windowPrefab);
+            window1.transform.position = new Vector3(
+                width / 2,
+                0,
+                0
+            );
+            window1.transform.parent = plex.transform;
+
+            GameObject window2 = Instantiate(windowPrefab);
+            window2.transform.position = new Vector3(
+                0,
+                0,
+                0
+            );
+            window2.transform.parent = plex.transform;
+
+            //GameObject window3 = Instantiate(windowPrefab);
+            //window3.transform.position = new Vector3(
+            //    1,
+            //    0,
+            //    0
+            //);
+            //window3.transform.parent = plex.transform;
+        }
+
+        return plex;
+    }
+}
