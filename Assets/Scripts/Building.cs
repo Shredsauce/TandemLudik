bb96f4a53de4d27626b4a6324f33bf7ac99a381b
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2019-02-07 12:46:38 -0500

    Added building class. Testing continuous builing generation. The seconds to wait in order to generate the next building will be determined by the width of the building (Building.width) and how fast the world is scrolling (GameControl.Instance.scrollSpeed)

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    [SerializeField] private float width = 5f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ScrollingScenery.cs
+++ Assets/Scripts/ScrollingScenery.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 
 public class ScrollingScenery : MonoBehaviour {
 
-    [SerializeField] private GameObject buildingPrefab;
+    [SerializeField] private Building buildingPrefab;
 
     [SerializeField] private Vector3 position;
     [SerializeField] private Vector3 rotation;
     [SerializeField] private float scale = 0.1f;
 
+    void Start () {
+        StartCoroutine(PlaceNextBuilding(null));
+    }
 
     void Update () {
         if (Input.GetKeyDown(KeyCode.L)) {
-            Debug.Log("Load building");
 
-            GameObject building = Instantiate(buildingPrefab);
-            GameObject container = new GameObject("Building");
-            // Make sure object is destroyed after it's out of view
-            container.AddComponent<Obstacle>();
-
-            building.transform.rotation = Quaternion.Euler(rotation);
-            building.transform.SetParent(container.transform);
-
-            container.transform.position = position;
-            container.transform.localScale = new Vector3(scale, scale, scale);
         }
 
+    }
+
+    private IEnumerator PlaceNextBuilding (Building prevBuilding) {
+
+        // TODO: Hardcoded for testing
+        float secondsToWait = 0.23f;
+
+        // TODO: Figure out how many seconds to wait based on
+        // 1. The length of the previous building prevBuilding
+        // 2. How fast the world is scrolling past GameControl.Instance.scrollSpeed
+        yield return new WaitForSeconds(secondsToWait);
+
+        Debug.Log("Load building");
+        Building building = Instantiate(buildingPrefab);
+        GameObject container = new GameObject("Building");
+        // Make sure object is destroyed after it's out of view
+        container.AddComponent<Obstacle>();
+
+        building.transform.rotation = Quaternion.Euler(rotation);
+        building.transform.SetParent(container.transform);
+
+        container.transform.position = position;
+
+        Debug.Log(container.transform.position);
+        container.transform.localScale = new Vector3(scale, scale, scale);
+
+        StartCoroutine(PlaceNextBuilding(building));
     }
 }
